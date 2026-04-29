f1e4e5f46fc43e16d141d24b3bb596986ae7fd83
Cyronp <vitorhenriquedasilveira@gmail.com>
2026-04-29 18:11:43 -0300

    feat: update Home component to display user prompt and input field

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+"use client"
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-black font-sans">
+      <div className="inline-flex gap-2">
       <h1>
-        Teste
+        visitor@visitor-PC:~$
       </h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input autoFocus className="focus:outline-none focus:ring-0 focus:border-transparent"></input>
+      </form>
+      </div>
     </div>
   );
 }
