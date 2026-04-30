223d8c5a08a2d35958c17611c01ed162f50da076
Cyronp <vitorhenriquedasilveira@gmail.com>
2026-04-29 23:03:17 -0300

    feat: implement terminal input handling in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
 "use client"
 
+import { useState } from "react";
+
 export default function Home() {
+  const [terminalInput, setTerminalInput] = useState("")
+    const handleKeyDown = (event: any) => {
+    if (event.key === 'Enter'){
+      console.log(terminalInput)
+    }
+  }
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-black font-sans">
       <div className="inline-flex gap-2">
-      <h1>
+      <span>
         visitor@visitor-PC:~$
-      </h1>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input autoFocus className="focus:outline-none focus:ring-0 focus:border-transparent"></input>
-      </form>
+      </span>
+        <input autoFocus className="focus:outline-none focus:ring-0 focus:border-transparent" onKeyDown={handleKeyDown} onChange={(e) => setTerminalInput(e.target.value)}/>
       </div>
     </div>
   );
